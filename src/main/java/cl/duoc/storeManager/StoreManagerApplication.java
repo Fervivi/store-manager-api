55e9b80daafb4eda3223e287e4baac682862636f
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-17 18:54:23 -0400

    first commit

---
+++ src/main/java/cl/duoc/storeManager/StoreManagerApplication.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.storeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagerApplication.class, args);
+    }
+}
