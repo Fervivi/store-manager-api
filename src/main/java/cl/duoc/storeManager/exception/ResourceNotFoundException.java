4aa5b3c3b9a76d20c705f51bbced5c7a7e68deb8
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-18 15:06:25 -0400

    feat: add ResourceNotFoundException class

---
+++ src/main/java/cl/duoc/storeManager/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.storeManager.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
