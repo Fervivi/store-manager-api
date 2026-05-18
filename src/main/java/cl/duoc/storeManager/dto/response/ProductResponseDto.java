9ad3978bda32f1d75b5576ee025ba03e526f493f
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-18 14:29:59 -0400

    feat: add ProductResponseDto class

---
+++ src/main/java/cl/duoc/storeManager/dto/response/ProductResponseDto.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.storeManager.dto.response;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponseDto {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+    private Integer stock;
+    private String categoria;
+    private Boolean activo;
+}
