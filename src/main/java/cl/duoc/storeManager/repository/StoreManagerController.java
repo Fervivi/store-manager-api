4f8a397bb6da237a61bf5bc0bacff04660d49e1e
fernando <fe.villalobosv@duocuc.cl>
2026-05-17 22:42:30 -0400

    feat: add repository class

---
+++ src/main/java/cl/duoc/storeManager/repository/StoreManagerController.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.storeManager.repository;
+
+public interface StoreManagerController {}
