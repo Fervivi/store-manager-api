e427c629348f2f50066fcdf32a4ee65982ac05d8
fernando <fe.villalobosv@duocuc.cl>
2026-05-17 22:46:14 -0400

    feat: model class

---
+++ src/main/java/cl/duoc/storeManager/model/StoreManagerModel.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.storeManager.model;
+
+public class StoreManagerModel {}
