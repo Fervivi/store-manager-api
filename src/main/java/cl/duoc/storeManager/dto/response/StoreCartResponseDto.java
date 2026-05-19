38c2d2cf350095931e0f9df843d65886a6f27226
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-19 19:35:11 -0400

    feat: add StoreCartResponseDto clas

---
+++ src/main/java/cl/duoc/storeManager/dto/response/StoreCartResponseDto.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.storeManager.dto.response;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoreCartResponseDto {
+
+    private Long productId;
+    private String nombreProducto;
+    private Integer cantidad;
+    private BigDecimal precioUnitario;
+    private BigDecimal subtotal;
+}
