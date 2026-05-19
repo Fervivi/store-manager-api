8adee8476170cafbfcfa9cc9ef0822bcd478c0dd
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-19 19:54:34 -0400

    feat: add endpoints to controller

--- src/main/java/cl/duoc/storeManager/controller/StoreManagerController.java
+++ src/main/java/cl/duoc/storeManager/controller/StoreManagerController.java
@@ -6,14 +6,20 @@
  */
 package cl.duoc.storeManager.controller;
 
-import cl.duoc.storeManager.dto.response.ProductResponseDto;
+import cl.duoc.storeManager.dto.request.AddProductToCartRequestDto;
+import cl.duoc.storeManager.dto.response.CartResponseDto;
+import cl.duoc.storeManager.dto.response.StoreCartResponseDto;
 import cl.duoc.storeManager.service.StoreManagerService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,13 +28,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class StoreManagerController {
     private final StoreManagerService storeManagerService;
 
-    @GetMapping("/products")
-    public ResponseEntity<List<Product>> getAllProducts() {
-        return new ResponseEntity.ok(storeManagerService.getAllProducts());
+    @PostMapping("/cart/products")
+    public CartResponseDto addProductToCart(@RequestBody AddProductToCartRequestDto request) {
+        return storeManagerService.addProductToCart(request);
     }
 
-    @GetMapping("/products/{id}")
-    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
-        return ResponseEntity.ok(storeManagerService.getProductById(id));
+    @GetMapping("/cart/{cartId}")
+    public List<StoreCartResponseDto> getCartWithProductDetails(@PathVariable Long cartId) {
+        return storeManagerService.getCartWithProductDetails(cartId);
+    }
+
+    @PutMapping("/cart/{cartId}/products/{productId}")
+    public CartResponseDto updateProductQuantity(
+            @PathVariable Long cartId, @PathVariable Long productId, @RequestParam Integer cantidad) {
+        return storeManagerService.updateProductQuantity(cartId, productId, cantidad);
+    }
+
+    @DeleteMapping("/cart/{cartId}/products/{productId}")
+    public void removeProductFromCart(@PathVariable Long cartId, @PathVariable Long productId) {
+        storeManagerService.removeProductFromCart(cartId, productId);
     }
 }
