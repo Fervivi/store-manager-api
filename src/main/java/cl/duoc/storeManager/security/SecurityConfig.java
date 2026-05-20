0a2cbcc90148a75b5d81786508f5eba417e7b4f6
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-19 20:29:04 -0400

    feat: add SecurityConfig class

--- src/main/java/cl/duoc/storeManager/security/SecurityConfig.java
+++ src/main/java/cl/duoc/storeManager/security/SecurityConfig.java
@@ -9,10 +9,15 @@ package cl.duoc.storeManager.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter {
+public class SecurityConfig {
+
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
