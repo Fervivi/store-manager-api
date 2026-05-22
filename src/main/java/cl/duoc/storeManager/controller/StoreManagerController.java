28d6fe3a7e85acc77eefee6219230f855fcae055
fernando <fe.villalobosv@duocuc.cl>
2026-05-22 17:49:18 -0400

    fix: fix errors

--- src/main/java/cl/duoc/storeManager/controller/StoreManagerController.java
+++ src/main/java/cl/duoc/storeManager/controller/StoreManagerController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/store")
+@RequestMapping("/api/vi/store")
 @RequiredArgsConstructor
 public class StoreManagerController {
     private final StoreManagerService storeManagerService;
