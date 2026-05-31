8e4d5bf938320e9cb6e024a274fb04fe308069a7
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-31 17:01:24 -0400

    chore: add CartUpdateRequest class

---
+++ src/main/java/cl/duoc/storeManager/dto/request/CartUpdateRequest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.storeManager.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartUpdateRequest {
+    private Long customerId;
+}
