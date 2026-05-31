e640e77fed0b49f4460f003b6755b4c9317bdcc5
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-31 16:59:54 -0400

    chore: add CartCreationRequest class

--- src/main/java/cl/duoc/storeManager/client/CartClient.java
+++ src/main/java/cl/duoc/storeManager/client/CartClient.java
@@ -6,9 +6,9 @@
  */
 package cl.duoc.storeManager.client;
 
-import cl.duoc.storeManager.dto.request.CartItemRequestDto;
 import cl.duoc.storeManager.dto.response.CartResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,10 +18,10 @@ public class CartClient {
 
     private final WebClient webClientCart;
 
-    public CartResponseDto addItemToCart(Long cartId, CartItemRequestDto request, String token) {
+    public CartResponseDto createCart(CartCreationRequest request, String token) {
         return webClientCart
                 .post()
-                .uri("/api/v1/carts/{cartId}/items", cartId)
+                .uri("")
                 .header("Authorization", token)
                 .bodyValue(request)
                 .retrieve()
@@ -32,17 +32,27 @@ public class CartClient {
     public CartResponseDto getCartById(Long cartId, String token) {
         return webClientCart
                 .get()
-                .uri("/api/v1/carts/{cartId}", cartId)
+                .uri("/{id}", cartId)
                 .header("Authorization", token)
                 .retrieve()
                 .bodyToMono(CartResponseDto.class)
                 .block();
     }
 
-    public CartResponseDto updateItem(Long cartId, Long itemId, CartItemRequestDto request, String token) {
+    public List<CartResponseDto> getAllCarts(String token) {
+        return webClientCart
+                .get()
+                .uri("")
+                .header("Authorization", token)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<CartResponseDto>>() {})
+                .block();
+    }
+
+    public CartResponseDto updateCart(Long cartId, CartUpdateRequest request, String token) {
         return webClientCart
                 .put()
-                .uri("/api/v1/carts/{cartId}/items/{itemId}", cartId, itemId)
+                .uri("/{id}", cartId)
                 .header("Authorization", token)
                 .bodyValue(request)
                 .retrieve()
@@ -50,13 +60,13 @@ public class CartClient {
                 .block();
     }
 
-    public CartResponseDto removeItemFromCart(Long cartId, Long itemId, String token) {
-        return webClientCart
+    public void deleteCart(Long cartId, String token) {
+        webClientCart
                 .delete()
-                .uri("/api/v1/carts/{cartId}/items/{itemId}", cartId, itemId)
+                .uri("/{id}", cartId)
                 .header("Authorization", token)
                 .retrieve()
-                .bodyToMono(CartResponseDto.class)
+                .toBodilessEntity()
                 .block();
     }
 }
