8db1df09471bde8ec22014b95ca7e4be2fdb6eac
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-31 17:19:03 -0400

    chore: add validation to CartCreationRequest class

--- src/main/java/cl/duoc/storeManager/dto/request/CartCreationRequest.java
+++ src/main/java/cl/duoc/storeManager/dto/request/CartCreationRequest.java
@@ -6,6 +6,8 @@
  */
 package cl.duoc.storeManager.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartCreationRequest {
+    @NotNull(message = "El id no debe es obligatorio")
+    @Positive(message = "el id debe ser positivo")
     private Long customerId;
 }
