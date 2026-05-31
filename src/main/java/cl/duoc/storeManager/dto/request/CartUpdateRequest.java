93abd9698a299ae5395ab469435141dbf091bf83
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-31 17:30:36 -0400

    feat: add validation to CartUpdateRequest class

--- src/main/java/cl/duoc/storeManager/dto/request/CartUpdateRequest.java
+++ src/main/java/cl/duoc/storeManager/dto/request/CartUpdateRequest.java
@@ -6,6 +6,8 @@
  */
 package cl.duoc.storeManager.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartUpdateRequest {
+    @NotNull(message = "El id es obligatorio")
+    @Positive(message = "El id debe ser positivo")
     private Long customerId;
 }
